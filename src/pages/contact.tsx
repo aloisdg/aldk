9e0b86ebc9905925a581eea87e5748e23def8bfe
Aloïs de Gouvello <aloisdegouvello@live.fr>
2023-12-21 10:16:31 +0100

    Add country and city

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -43,6 +43,30 @@ const Contact = () => {
               />
             </label>
           </p>
+           <p>
+            <label htmlFor="name" className="block">
+              <span className="text-gray-700">Quel est votre pays ?</span>
+              <input
+                id="pays"
+                name="pays"
+                className="form-input mt-1 block w-full border-blue-500 border-b-2"
+                placeholder="France, Belgique, Palestine, Tibet, etc."
+                aria-label="pays"
+              />
+            </label>
+          </p>
+           <p>
+            <label htmlFor="name" className="block">
+              <span className="text-gray-700">Quel est votre ville et code postal ?</span>
+              <input
+                id="ville"
+                name="ville"
+                className="form-input mt-1 block w-full border-blue-500 border-b-2"
+                placeholder="Nantes 44000"
+                aria-label="ville"
+              />
+            </label>
+          </p>
           <button
             className="w-full sm:w-auto flex-none bg-blue-500 hover:bg-blue-700 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-900 focus:outline-none transition-colors duration-200"
             type="submit"
