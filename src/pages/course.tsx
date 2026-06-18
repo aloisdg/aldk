4940fe2cedd5dd7e77fc96a686093e32e6e2132e
Aloïs de Gouvello <aloisdegouvello@live.fr>
2026-06-18 10:35:50 +0200

    Create course.tsx

---
+++ src/pages/course.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import { Meta } from "../layout/Meta";
+import { Main } from "../templates/Main";
+
+const Course = () => {
+  return (
+    <Main
+      meta={
+        <Meta
+          title="formation"
+          description="Demande de formation"
+        />
+      }
+    >
+      <div className="sm:max-w-screen-md sm:mx-auto">
+Omnis voluptatem ea minus deleniti tempora facilis excepturi.
+Consectetur corrupti quae adipisci eum nostrum. Accusantium iure labore velit assumenda non aut consequuntur.
+
+Qui dicta est voluptate est libero vitae perferendis.
+Ad voluptas modi nemo quam tenetur maxime ullam iure.
+Quam ut repudiandae alias saepe omnis nemo. Sapiente ea eos maiores praesentium laboriosam quasi.
+
+Sapiente consequatur numquam aspernatur est officia pariatur voluptatem.
+Libero provident cum itaque vero qui incidunt placeat. Hic blanditiis autem corporis.
+Laborum ut amet et unde aut dolor quia. Nobis saepe velit et.
+
+Occaecati eligendi sed odio eum deserunt rem et placeat.
+Occaecati voluptatem voluptatem dolores dignissimos possimus.
+Sequi id voluptatem veniam.
+
+Delectus ducimus autem eveniet tenetur voluptas iste.
+Quasi cumque enim qui ea incidunt sunt.
+Eum possimus voluptas illum ipsa dolores nostrum asperiores.
+Accusantium id dolores quae maiores.
+Laudantium qui quia corporis illo voluptas.
+      </div>
+    </Main>
+  );
+};
+
+export default Course;
